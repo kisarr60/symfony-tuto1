f8415de833936bdeda2d42fd5d0acd0deb02ccd1
Ibrahima Sarr <wpibousarr@gmail.com>
2020-07-22 11:08:00 +0000

    first commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Articles;
+use Symfony\Component\HttpFoundation\Request; // Nous avons besoin d'accéder à la requête pour obtenir le numéro de page
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
+
+/**
+ * Class ArticlesController
+ * @package App\Controller
+ * @Route("/actualites", name="actualites_")
+ */
+
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/", name="articles")
+     */
+    public function index(Request $request, PaginatorInterface $paginator) // Nous ajoutons les paramètres requis
+    {
+        // Méthode findBy qui permet de récupérer les données avec des critères de filtre et de tri
+        $donnees = $this->getDoctrine()->getRepository(Articles::class)->findBy([],['created_at' => 'desc']);
+
+        $articles = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            4 // Nombre de résultats par page
+        );
+        //dd($articles);
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+    /**
+     * @Route("/{slug}", name="article")
+    */
+    public function article($slug)
+    {
+        // On récupère l'article correspondant au slug
+        $article = $this->getDoctrine()->getRepository(Articles::class)->findOneBy(['slug' => $slug]);
+        if(!$article){
+            // Si aucun article n'est trouvé, nous créons une exception
+            throw $this->createNotFoundException('L\'article n\'existe pas');
+        }
+        // Si l'article existe nous envoyons les données à la vue
+        return $this->render('articles/article.html.twig', compact('article'));
+    }
+
+}
